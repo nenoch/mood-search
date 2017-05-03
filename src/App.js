8014097e90f9f1a9ac3a3ebe3564fef417dcf3e1
Irene Canuti <irene.canuti@gmail.com>
2017-05-03 10:42:38 +0100

    Set state and Implement state change via input
    - Set mutable state in App.js
    - Add functions to handle user's input and change state
    in App.js and Search.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import SearchText from './SearchText';
 
 class App extends React.Component {
-  render() {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: ""
+    }
+
+    this.handleTextInput = this.handleTextInput.bind(this);
+  }
+
+  handleTextInput(newText){
+    this.setState({ text: newText });
+  }
+
+  render(){
     return (
       <div className='app'>
         <h1>Mood Search!</h1>
         <h3>Type in some text and find out how you feel today.</h3>
-        <SearchText />
+        <SearchText text={this.state.text} onTextInput={this.handleTextInput}/>
       </div>
     );
   }
--- src/SearchText.js
+++ src/SearchText.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class SearchText extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleNewInput = this.handleNewInput.bind(this);
+  }
+
+  handleNewInput(event){
+    this.props.onTextInput(event.target.value);
+  }
+
   render() {
     return (
       <div>
         <form>
-          <label>Text</label>
-          <input type="input" name="user-input" value="" />
+          <input type="input"
+          placeholder="Type your sentence here..."
+          value={this.props.text}
+          onChange={this.handleNewInput} />
           <button type="submit">Analyse!</button>
         </form>
       </div>
